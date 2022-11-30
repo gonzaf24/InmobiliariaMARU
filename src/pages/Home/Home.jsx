d64418368ddf3bc5298d0e750d29be4e918facbe
“TTK-gonzalo” <gonzalo.fraschini@myowncto.com>
2022-11-30 16:48:00 +0100

    translations added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,37 +1,49 @@
-import React from 'react';
-import classnames from 'classnames';
-import PropTypes from 'prop-types';
+import React from "react";
+import classnames from "classnames";
+import PropTypes from "prop-types";
 
-import styles from './Home.module.scss';
+import styles from "./Home.module.scss";
+import { useTranslation } from "react-i18next";
 
-const propTypes={
+const propTypes = {
   className: PropTypes.string,
   testId: PropTypes.string,
   id: PropTypes.string,
 };
 
-const defaultProps={
-  className: '',
+const defaultProps = {
+  className: "",
   testId: undefined,
   id: undefined,
 };
 
-const Home=({ className, testId, id, texts: textsProp }) => {
+const texts = {
+  TestText: "Home.TestText",
+  ChangeLanguage: "Home.ChangeLanguage",
+}
 
-  const homeClassNames=classnames(styles.Home, className);
+const Home = ({ className, testId, id, texts: textsProp }) => {
+  const homeClassNames = classnames(styles.Home, className);
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = () => {
+    const lang = i18n.language === "en" ? "es" : "en";
+    i18n.changeLanguage(lang);
+  };
 
   return (
-    <div
-      className={ homeClassNames }
-      testid={ testId }
-      id={ id }
-    >
-      <span>Text  Home </span>
+    <div className={homeClassNames} testid={testId} id={id}>
+      <br/><br/><br/>
+      <span>{t(texts.TestText)}</span>
+      <br/><br/><br/>
+      <button onClick={changeLanguage}>{t(texts.ChangeLanguage)}</button>
+      <br/><br/><br/>
+      <span>{`Lenguaje:  ${i18n.language}`}</span>
     </div>
   );
 };
 
-Home.propTypes=propTypes;
-Home.defaultProps=defaultProps;
+Home.propTypes = propTypes;
+Home.defaultProps = defaultProps;
 
 export default Home;
