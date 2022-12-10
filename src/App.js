8e3be14aa4d5bf31b5b630c74d9f845f2e3b62b0
“TTK-gonzalo” <gonzalo.fraschini@myowncto.com>
2022-12-10 05:03:55 +0100

    navbar and language added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Suspense } from 'react';
 import './styles/theme.scss';
 import './App.scss';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home/Home';
 import About from './pages/About/About';
@@ -9,6 +9,7 @@ import { ToastProvider } from './context/toastContext';
 import { UserProvider } from './context/userContext';
 import Login from './pages/Login/Login';
 import Users from './pages/Admin/Users';
+import { NavigationBar } from './components';
 
 function App() {
 	return (
@@ -16,29 +17,13 @@ function App() {
 			<ToastProvider>
 				<Suspense fallback={null}>
 					<Router>
-						<div>
-							<ul>
-								<li>
-									<Link to='/'>Home</Link>
-								</li>
-								<li>
-									<Link to='/about'>About</Link>
-								</li>
-								<li>
-									<Link to='/login'>Login</Link>
-								</li>
-								<li>
-									<Link to='/users'>Users</Link>
-								</li>
-							</ul>
-							<hr />
-							<Routes>
-								<Route path='/' element={<Home />} />
-								<Route path='/about' element={<About />} />
-								<Route path='/login' element={<Login />} />
-								<Route path='/users' element={<Users />} />
-							</Routes>
-						</div>
+						<NavigationBar />
+						<Routes>
+							<Route path='/' element={<Home />} />
+							<Route path='/about' element={<About />} />
+							<Route path='/login' element={<Login />} />
+							<Route path='/users' element={<Users />} />
+						</Routes>
 					</Router>
 				</Suspense>
 			</ToastProvider>
---
+++ src/components/LanguageSelector/LanguageSelector.jsx
@@ -0,0 +1,77 @@
+import React, { useState, useRef, useEffect } from 'react';
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
+
+import styles from './LanguageSelector.module.scss';
+import { MdLanguage } from 'react-icons/md';
+import { useTranslation } from 'react-i18next';
+import { EnFlag, EsFlag } from '../../assets/icons';
+import { LANGUAGES } from '../../utils/constants';
+
+const propTypes = {
+	className: PropTypes.string,
+	testId: PropTypes.string,
+	id: PropTypes.string,
+};
+
+const defaultProps = {
+	className: '',
+	testId: undefined,
+	id: undefined,
+};
+
+const LanguageSelector = ({ className, testId, id }) => {
+	const languageSelectorClassNames = classnames(styles.LanguageSelector, className);
+	const { i18n } = useTranslation();
+	const language = i18n.language;
+	const refSowFlags = useRef();
+	const refLanguage = useRef();
+	const [showFlags, setShowFlags] = useState(false);
+
+	const engClassNames = classnames(styles.FlagIcon, {
+		[styles.FlagActive]: language === LANGUAGES.ENGLISH,
+	});
+
+	const espClassNames = classnames(styles.FlagIcon, {
+		[styles.FlagActive]: language === LANGUAGES.SPANISH,
+	});
+
+	const showFlagWrapper = classnames(styles.FlagWrapper, {
+		[styles.ShowFlagWrapper]: showFlags,
+	});
+
+	const handleLanguageChange = language => {
+		i18n.changeLanguage(language);
+		setShowFlags(false);
+	};
+
+	const onClickOutside = event => {
+		if (!refSowFlags.current?.contains(event.target) && !refLanguage.current?.contains(event.target)) {
+			setShowFlags(false);
+		}
+	};
+
+	useEffect(() => {
+		document.addEventListener('mousedown', onClickOutside);
+		return () => {
+			document.removeEventListener('mousedown', onClickOutside);
+		};
+	}, []);
+
+	return (
+		<div className={languageSelectorClassNames} data-testid={testId} id={id}>
+			<div onClick={() => setShowFlags(!showFlags)} ref={refLanguage}>
+				<MdLanguage className={styles.Language} />
+			</div>
+			<div className={showFlagWrapper} ref={refSowFlags}>
+				<EnFlag className={engClassNames} onClick={() => handleLanguageChange(LANGUAGES.ENGLISH)} />
+				<EsFlag className={espClassNames} onClick={() => handleLanguageChange(LANGUAGES.SPANISH)} />
+			</div>
+		</div>
+	);
+};
+
+LanguageSelector.propTypes = propTypes;
+LanguageSelector.defaultProps = defaultProps;
+
+export default LanguageSelector;
