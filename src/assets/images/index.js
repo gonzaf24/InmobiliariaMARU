bfe79562e25f192d07a2a8656db7fb07cba1e31e
“TTK-gonzalo” <gonzalo.fraschini@myowncto.com>
2023-06-04 01:28:50 +0200

    slider

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,4 +1,5 @@
 import BlueLogo from './blue-logo.png';
 import GreenLogo from './green-logo.png';
+import NoImageAvailable from './no-image-available.png';
 
-export { BlueLogo, GreenLogo };
+export { BlueLogo, GreenLogo, NoImageAvailable };
--- src/components/index.js
+++ src/components/index.js
@@ -3,10 +3,11 @@ import LanguageSelector from './LanguageSelector';
 import FullSizeImage from './FullSizeImage';
 import Switch from './Switch';
 import Image from './Image';
+import Slider from './Slider';
 
 export * from './files';
 export * from './modals';
 export * from './maps';
 export * from './utils';
 
-export { Loader, LanguageSelector, FullSizeImage, Switch, Image };
+export { Loader, LanguageSelector, FullSizeImage, Switch, Image, Slider };
