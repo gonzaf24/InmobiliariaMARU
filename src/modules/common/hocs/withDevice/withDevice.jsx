5ad1daac62dbd915143dcf4ac2056ccfd0e3df50
“TTK-gonzalo” <gonzalo.fraschini@myowncto.com>
2023-06-15 01:14:26 +0200

    more changes

--- src/modules/common/hocs/withDevice/withDevice.jsx
+++ src/modules/common/hocs/withDevice/withDevice.jsx
@@ -1,7 +1,6 @@
 import React, { forwardRef, useMemo } from 'react';
 import PropTypes from 'prop-types';
-
-import { useDevice } from 'modules/common/hooks';
+import { useDevice } from '../../hooks';
 
 const propTypes = {
 	bigDesktopComponent: PropTypes.elementType,
